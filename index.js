af19c531868f2aa7006bdaf9a10076dac82b0167
kathiravanjoythi <kathiravanjoythi@gmail.com>
2021-10-16 21:20:17 +0530

    switch statement

--- index.js
+++ index.js
@@ -44,12 +44,45 @@ let mymark = 20;
 // }
 
 
-if(mymark >= 80) {
-  console.log("Pass and firstclass")
-} else if(mymark >= 60) {
-  console.log("pass and second class")
-} else if(mymark >= 30) {
-  console.log("pass")
-} else {
-  console.log('fail')
+// if(mymark >= 80) {
+//   console.log("Pass and firstclass")
+// } else if(mymark >= 60) {
+//   console.log("pass and second class")
+// } else if(mymark >= 30) {
+//   console.log("pass")
+// } else {
+//   console.log('fail')
+// }
+
+// switch statement
+let today = new Date()
+
+// Sunday Monday Tuesday Wednesday Tuesday Friday Satureday
+console.log(today.getDay()) 
+
+switch (today.getDay()) {
+  case 1:
+    console.log('Monday');
+    break;
+  case 2:
+    console.log('Tuesday');
+    break;
+  case 3:
+    console.log('Wednesday');
+    break;
+  case 4:
+    console.log('Tuesday');
+    break;
+  case 5:
+    console.log('Friday');
+    break;
+  case 6:
+    console.log('Satureday');
+    break;
+  case 7:
+    console.log('Sunday');
+    break;
+  default:
+    console.log('nothing is match')
+    break
 }
