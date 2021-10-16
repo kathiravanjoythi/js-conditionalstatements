417e19dcc15c0840ff1116fda7727a5d4e41cc82
kathiravanjoythi <kathiravanjoythi@gmail.com>
2021-10-16 20:57:49 +0530

    if , if else, else if

--- index.js
+++ index.js
@@ -4,3 +4,52 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+// if
+let isLightison = false;
+
+if(isLightison == false) {
+  console.log("Light is off")
+} else {
+  console.log("Light is on")
+}
+
+// if else
+let myname = 'kathir';
+
+if(myname == 'kumaran') {
+  console.log("your name is kumaran") 
+} else {
+  console.log('your name is kathir')
+}
+
+// else if
+
+let mymark = 20;
+
+// 30 > pass
+// 30 < fail
+// 80 > firstclass
+// 60 <= second class
+
+// if(mymark >= 30) {
+//   console.log("pass")
+// } else if(mymark >= 30 &&  mymark >= 80) { 
+//   console.log("Pass and firstclass")
+// } else if(mymark <= 60) {
+//   console.log("pass and second class")
+// }
+// else { 
+//   console.log('fail')
+// }
+
+
+if(mymark >= 80) {
+  console.log("Pass and firstclass")
+} else if(mymark >= 60) {
+  console.log("pass and second class")
+} else if(mymark >= 30) {
+  console.log("pass")
+} else {
+  console.log('fail')
+}
